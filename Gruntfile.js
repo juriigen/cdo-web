199950c6bacd61f1c57e25f156968a633dd258ea
Robert Blust <robert.blust@flatland.ch>
2015-01-04 19:25:17 +0100

    added login views to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -268,7 +268,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: ['*.html', 'views/{,*/}*.html'],
+          src: ['*.html', 'views/{,*/}*.html', 'modules/authentication/views/{,*/}*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
@@ -307,6 +307,7 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
+            'modules/authentication/views/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'fonts/{,*/}*.*'
           ]
