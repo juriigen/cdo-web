e39fa5013f8906a486d6879783f41aea8e3a8c50
Robert Blust <robert.blust@flatland.ch>
2015-01-11 17:54:26 +0100

    info panel json

--- app/scripts/controllers/repo.js
+++ app/scripts/controllers/repo.js
@@ -8,11 +8,17 @@
  * Controller of the cdoWebApp
  */
 angular.module('cdoWebApp')
-  .controller('RepoCtrl', function ($scope, $log) {
+  .controller('RepoCtrl', function ($scope, $log, RepoAccessService) {
     var repo = this;
     $scope.$on('objectSelected', function (scope, data) {
       repo.selectedObject = data;
 
-      $log.debug('TreeCtrl.objectSelected - received event - id ' + repo.selectedObject.id);
+      $log.debug('RepoCtrl.objectSelected - received event - id ' + repo.selectedObject.id);
+
+      RepoAccessService.get(repo.selectedObject._links.self.href + '?crefs&rrefs&meta', function (data, status) {
+        if (status === 200) {
+          repo.selectedObject = data.data
+        }
+      });
     });
   });
