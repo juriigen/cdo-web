adf577e3dffdad56568551f23c33f194d8b305be
Robert Blust <robert.blust@flatland.ch>
2015-01-14 08:51:58 +0100

    cleanup

--- app/scripts/controllers/repoTree.js
+++ app/scripts/controllers/repoTree.js
@@ -9,12 +9,9 @@
  */
 angular.module('cdoWebApp')
   .controller('RepoTreeCtrl', function ($rootScope, $scope, $log, TreeModelService, RepoAccessService, ContextService) {
-    var newId = 0;
+
     var repoTree = this;
-
-    repoTree.newNode = {};
-
-    //repoTree.selectedObject;
+    repoTree.treeData = [];
 
     repoTree.treeConfig = {
       core : {
@@ -29,39 +26,17 @@ angular.module('cdoWebApp')
         /* jshint camelcase:true */
         worker : true
       },
-      plugins : [ 'wholerow' ],
-      version : 1
+      plugins : [ 'wholerow' ]
     };
 
     repoTree.readyCB = function() {
       $log.debug('RepoTreeCtrl.readyCB');
 
       if ($rootScope.globals.currentUser !== undefined) {
-
+        // page reload
         RepoAccessService.get('/node?crefs', function (data, status) {
           if (status === 200) {
-            repoTree.treeData.length = 0;
-
-            if (data.data.references.contents !== undefined) {
-
-              var children = TreeModelService.transformChildren(data.data.references.contents, '#');
-              children.forEach(function(node) {
-                repoTree.treeData.push(node);
-
-                RepoAccessService.get(node.url + '/references?crefs', function (data, status) {
-                  if (status === 200) {
-
-                    var children = TreeModelService.transformChildren(data.data, node.id);
-                    children.forEach(function(node) {
-                      repoTree.treeData.push(node);
-                    });
-                    node.resolved = true;
-                  } else if (status === 404) {
-                    node.resolved = true;
-                  }
-                });
-              });
-            }
+            $scope.$broadcast('repoRootNodeUpdated', data);
           }
         });
       }
@@ -78,16 +53,6 @@ angular.module('cdoWebApp')
     repoTree.applyModelChanges = function() {
       $log.debug('RepoTreeCtrl.applyModelChanges');
       return true;
-    };
-
-    repoTree.isAddNodeDisabled = function() {
-      if (repoTree.newNode.parent === undefined) {
-        return true;
-      }
-      if (repoTree.newNode.text === undefined) {
-        return true;
-      }
-      return false;
     };
 
     repoTree.beforeOpenNodeCB = function(e, item) {
@@ -115,35 +80,31 @@ angular.module('cdoWebApp')
           });
         }
       });
-
     };
 
-    repoTree.addNewNode = function() {
-      repoTree.treeData.push({ id : (newId++).toString(), parent : repoTree.newNode.parent, text : repoTree.newNode.text });
-    };
-
-    repoTree.treeData = [];
-
+    // either login or reload is triggered
     $scope.$on('repoRootNodeUpdated', function (scope, data) {
       $log.debug('RepoTreeCtrl.repoRootNodeUpdated - received event');
 
       repoTree.treeData.length = 0;
-      var children = TreeModelService.transformChildren(data.data.references.contents, '#');
-      children.forEach(function(node) {
-        repoTree.treeData.push(node);
-        RepoAccessService.get(node.url + '/references?crefs', function (data, status) {
-          if (status === 200) {
 
-            var children = TreeModelService.transformChildren(data.data, node.id);
-            children.forEach(function(node) {
-              repoTree.treeData.push(node);
-            });
-            node.resolved = true;
-          } else if (status === 404) {
-            node.resolved = true;
-          }
+      if (data.data.references.contents !== undefined) {
+        var children = TreeModelService.transformChildren(data.data.references.contents, '#');
+        children.forEach(function(node) {
+          repoTree.treeData.push(node);
+          RepoAccessService.get(node.url + '/references?crefs', function (data, status) {
+            if (status === 200) {
+
+              var children = TreeModelService.transformChildren(data.data, node.id);
+              children.forEach(function(node) {
+                repoTree.treeData.push(node);
+              });
+              node.resolved = true;
+            } else if (status === 404) {
+              node.resolved = true;
+            }
+          });
         });
-      });
+      }
     });
-
   });
