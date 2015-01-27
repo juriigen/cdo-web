ae6c447f245df1a47312df005cff92e02f9d1d2b
Robert Blust <robert.blust@flatland.ch>
2015-01-27 15:28:19 +0100

    localhost

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -51,6 +51,7 @@ angular
     $httpProvider.defaults.withCredentials = true;
   })
   .config(function($logProvider){
+    //$logProvider.debugEnabled(false);
     $logProvider.debugEnabled(true);
   })
   .run(['$rootScope', '$location', '$cookieStore', '$http',
@@ -69,6 +70,7 @@ angular
       });
 
       /* Global Variables */
+      $rootScope.endpoint = 'https://cdo-flatland.rhcloud.com';
       $rootScope.endpoint = 'https://localhost:9080';
-      $rootScope.dateFormat = 'dd MMMM yyyy - HH:mm:ss';
+      //$rootScope.dateFormat = 'dd MMMM yyyy - HH:mm:ss';
     }]);
