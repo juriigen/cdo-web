1c2d4bd27ab28dce6653f42513b75baa16becc8b
Robert Blust <robert.blust@flatland.ch>
2015-07-24 11:17:48 +0200

    first table

--- app/scripts/controllers/govattributes.js
+++ app/scripts/controllers/govattributes.js
@@ -8,7 +8,7 @@
  * Controller of the cdoWebApp
  */
 angular.module('cdoWebApp')
-  .controller('GovattributesCtrl', function ($scope, $log, RepoAccessService) {
+  .controller('GovattributesCtrl', function ($scope, $log, RepoAccessService, CalculateUrlService) {
 
     $scope.itemsByPage=1500;
 
@@ -18,10 +18,31 @@ angular.module('cdoWebApp')
 
       if (status === 200) {
         var result = data.data
-        $log.info(JSON.stringify(result))
+
         for (var j = 0; j < result.length; j++) {
-          $log.info(JSON.stringify(result[j]));
-          $scope.rowCollection.push(result[j]);
+
+          var entry = {};
+          entry.icon = CalculateUrlService.getUrl(result[j].icon);
+          entry.id = result[j].attributes.CDA_ID;
+          entry.name = result[j].attributes.name;
+          entry.id = result[j].attributes.CDA_ID;
+          entry.description = result[j].attributes.description;
+          if (result[j].references.owner !== undefined) {
+            entry.owner = result[j].references.owner.attributes.firstName + ' ' + result[j].references.owner.attributes.name;
+          }
+          if (result[j].references.deputy !== undefined) {
+            entry.deputy = result[j].references.deputy.attributes.firstName + ' ' + result[j].references.deputy.attributes.name;
+          }
+          if (result[j].references.ownerShipArea !== undefined) {
+            entry.area = result[j].references.ownerShipArea.attributes.name;
+          }
+          if (result[j].references.dataFamily !== undefined) {
+            entry.family = result[j].references.dataFamily.attributes.name;
+          }
+
+          entry.state = result[j].attributes.lifecycleState;
+
+          $scope.rowCollection.push(entry);
         }
 
       } else {
