9062e9890444d2578e62e328c7a5704257bf24cc
Robert Blust <robert.blust@flatland.ch>
2015-08-04 13:21:06 +0200

    fixed angular version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -131,8 +131,8 @@ angular
       /* Global Variables */
       //$rootScope.endpoint = 'https://cdo-flatland.rhcloud.com';
       //$rootScope.endpoint = 'http://192.168.0.127';
-      $rootScope.endpoint = 'https://63fd9c1b.ngrok.com';
-      //$rootScope.endpoint = 'http://localhost:8080';
+      //$rootScope.endpoint = 'https://63fd9c1b.ngrok.com';
+      $rootScope.endpoint = 'http://localhost:8080';
       $rootScope.repository = 'repo/';
       //$rootScope.endpoint = 'https://swisscom.3ap.ch';
       $rootScope.dateFormat = 'dd MMMM yyyy - HH:mm:ss';
